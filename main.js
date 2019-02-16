57a1d17814f1aa712085251bcc1481c8e9fa0254
claire <172678694@qq.com>
2019-02-16 23:38:38 +0800

    finish

--- main.js
+++ main.js
@@ -1,48 +1,119 @@
-let n
-初始化()
-setInterval(()=>{
-    makeLeave(getImage(n)).one('transitionend',(e)=>{
-         makeEnter($(e.currentTarget))
-     })
-    makeCurrent(getImage(n+1))
-    n +=1
-},3000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
+
+makeFakeSlides()
+$slides.hide().offset()
+$slides.css({transform:'translateX(-400px)'}).show()
+bindEvents()
+$(next).on('click',function(){
+    goToSlide(current+1)
+})
+$(previous).on('click',function(){
+    goToSlide(current-1)
+})
+
+let timer=setInterval(function(){
+    goToSlide(current+1)
+},2000)
+
+$('.container').on('mouseenter',function(){
+    window.clearInterval(timer)
+}).on('mouseleave',function(){
+    timer=setInterval(function(){
+        goToSlide(current+1)
+    },2000)
+})
 
 
 
 
 //工具函数
-
-
-function getImage(n){
-    return $(`.images>img:nth-child(${x(n)})`)
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
 }
 
-function x(n){
-    if (n>5){
-        n=n%5
-        if(n===0){
-            n=5
-        }
-    } //n=1 2 3 4 5
-    return n
+function bindEvents(){
+    $('#buttonWrapper').on('click','button',function(e){
+        let $button=$(e.currentTarget)
+        let index=$button.index()
+        goToSlide(index)
+    })
 }
 
-function 初始化(){
-    n=1
-    $(`.images >img:nth-child(${n})`).addClass('current')
-     .siblings().addClass('enter')
+
+//重要一步
+function goToSlide(index) {
+    if(index>$buttons.length-1){
+        index=0
+    }else if(index<0){
+        index=$buttons.length-1
+    }
+    if(current===$buttons.length-1 && index===0){
+        //最后一张到第一张
+        console.log(1)
+        $slides.css({transform:`translateX(${-(current+2)*400}px)`})
+            .one('transitionend',function(){
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+                .show()
+            })
+        
+    } else if(current===0 && index===$buttons.length-1){
+        //第一张到最后一张
+        console.log(2)
+        $slides.css({transform:'translateX(0px)'})
+            .one('transitionend',function(){
+                $slides.hide().offset()
+                $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+                .show()
+            })
+    }else{
+        console.log(3)
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+    }
+    current=index
 }
 
-function makeCurrent($node){
-    return $node.removeClass('enter ').addClass('current')
-}
 
-function makeLeave($node){
-    return $node.removeClass('current').addClass('leave')
-}
 
-function makeEnter($node){
-    return $node.removeClass('leave').addClass('enter')
-}
+
+// function bindEvents(){
+//     $buttons.eq(0).on('click',function(){
+//         if(current===2){
+//             $slides.css({transform:'translateX(-1600px)'})
+//             .one('transitionend',function(){
+//                 $slides.hide().offset()
+//                 $slides.css({transform:'translateX(-400px)'})
+//                 .show()
+//             })
+//         }else{
+//             $slides.css({transform:'translateX(-400px)'})
+//         }
+//         current=0
+//     })
+//     $buttons.eq(1).on('click',function(){
+//         $slides.css({transform:'translateX(-800px)'})
+//         current=1
+//     })
+//     $buttons.eq(2).on('click',function(){
+//         if(current===0){
+//             $slides.css({transform:'translateX(0px)'})
+//             .one('transitionend',function(){
+//                 $slides.hide().offset()
+//                 $slides.css({transform:'translateX(-1200px)'})
+//                 .show()
+//             })
+//         }else{
+//         $slides.css({transform:'translateX(-1200px)'})
+//         }
+//         current=2
+//     })
+
+// }
